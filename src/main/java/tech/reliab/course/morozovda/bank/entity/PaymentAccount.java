00b5c76d3f91b97252e1ce0ac70753da2bf66f3f
DanArmor <39347109+DanArmor@users.noreply.github.com>
2023-10-08 21:28:17 +0300

    feat: paymentaccount entity added

---
+++ src/main/java/tech/reliab/course/morozovda/bank/entity/PaymentAccount.java
@@ -0,0 +1,38 @@
+package main.java.tech.reliab.course.morozovda.bank.entity;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class PaymentAccount {
+    private BigDecimal balance;
+
+    private void initWithDefaults() {
+        balance = new BigDecimal("0");
+    }
+
+    public PaymentAccount() {
+        super();
+        initWithDefaults();
+    }
+
+    public PaymentAccount(UUID id, User user, Bank bank, BigDecimal balance) {
+        super(id, user, bank);
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " balance='" + getBalance() + "'" +
+                "}";
+    }
+
+    public BigDecimal getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+}
